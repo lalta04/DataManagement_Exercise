98d3d6fac72e560053c78544a6da8e8c0d51a683
lalta04 <142004100+lalta04@users.noreply.github.com>
2024-07-04 18:27:24 +0900

    Dice game: initial revision(サイコロゲーム：最初の見直し)

---
+++ ex4.py
@@ -0,0 +1,8 @@
+import random
+dice1 = random.randint(1,6)
+dice2 = random.randint(1,6)
+print("Rolling dice...")
+print("Die 1: "+str(dice1))
+print("Die 2: "+str(dice2))
+Total = dice1+dice2
+print("Total value: "+str(Total))
